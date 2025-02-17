6cb5f620998573c8111174a05c19393036a9f061
Caleb B <196969217+Caleb-Bish@users.noreply.github.com>
2025-02-17 15:24:04 +0000

    Solved problem 3.

--- C++/Problem_3/largestPrimeFactor.cpp
+++ C++/Problem_3/largestPrimeFactor.cpp
@@ -1,54 +1,95 @@
 /* The prime factors of 13195 are 5,7,13 and 29.
  * What is the largest prime factor of the number 600851475143?
  *
- * STATUS: IN PROGRESS.
+ * STATUS: SOLVED.
  */
 #include <cstdint>
 #include <iostream>
-#define FIND_PRIME 600851475143
+#include <chrono>
+#include <cmath>
+#include "largestPrimeFactor.h"
 
 
-// we find a factor of FIND_PRIME
-// then we calculate if that is a prime number
 
 
-bool primeFound(int potentialPrime) {
-    std::int32_t potentialPrimeCeiling { (potentialPrime / 2) };
 
-    for (std::int32_t counter = 2; counter < potentialPrimeCeiling; counter++) {
-        if ( potentialPrimeCeiling % counter == 0 ) {
-            // Non-prime found.
-            return false;
-        }
+// Calculate largest prime factor.
+// This can be calculated using the Sieve of Eratosthenes more efficiently, may be worth returning to.
+std::uint64_t largestPrimeFactor(std::uint64_t number) {
+    /* As we're trying to find the PRIME factors, can do the same trick as described
+     * within isPrime, where we calculate up to the sqrt of the value.
+     * If we were trying to find the largest factor in general, would need to
+     * calculate up to number / 2.
+     * Recall that in C++ any fractional part of the quotient is dropped, hence the +1.
+     */
+    const std::uint64_t primeCeiling { std::sqrt(number) + 1 };
+
+    // Check if the argument passed is itself prime.
+    if ( isPrime(number) ) {
+        return number;
     }
 
-    // A prime number has been found.
+    std::uint64_t largestFound {};
+
+    /* Since we're explicitly only looking for the largest prime factor,
+     * can do a negative loop and then break out of it.
+     */
+    for (std::uint64_t counter{primeCeiling}; counter >= 2; counter-=2) {
+        if ( number % counter == 0 && isPrime(counter)) {
+                largestFound = counter;
+                break;
+                }
+    }
+    return largestFound;
+}
+
+
+
+
+
+// Check if an integral is prime.
+bool isPrime (std::uint64_t potentialPrime) {
+
+    // If the potentialPrime is divisible by 2 (or equal to 2 itself) then it cannot be prime.
+    if (potentialPrime != 2 && potentialPrime % 2 == 0) {
+            return false;
+    }
+
+
+   /* Only need to calculate up to the square root of the potentialPrime to determine if it is prime.
+    * This is as if n is not a prime and as n = a*b, only 'a' OR 'b' can be > sqrt(n), as if a and b were both
+    * > sqrt(n) then a*b would be greater than sqrt(n)*sqrt(n) = n, which is impossible. Hence, if a number is
+    * composite then we will find at least one factor <= n, and if we don't then n is a prime.
+    */
+    std::int64_t potentialPrimeCeiling { std::sqrt(potentialPrime) + 1 };
+    for (std::uint64_t counter {3}; counter <= potentialPrimeCeiling; counter+=2) {
+        if (potentialPrime % counter == 0) {
+            return false;
+            }
+    }
     return true;
 }
 
 
 
+
+
 int main() {
+    const std::uint64_t findPrimeOf {600851475143};
+    // For timing how long this program takes to find the largest prime factor.
+    auto startPoint = std::chrono::high_resolution_clock::now();
 
-    // Logically, no prime factor can be greater than half of the value you are trying to find the prime factors of.
-    // Recall that in C++ any fractional part of the quotient is dropped, hence the +1.
-    std::int64_t primeCeiling { (FIND_PRIME / 2) + 1 };
 
-    // Performing a negative loop allows us to find the largest factor first, don't need to calculate every prime factor.
-    do {
-        // Check if the current highest potential value is a factor of FIND_PRIME.
-        if (primeCeiling % FIND_PRIME == 0) {
-            // If current highest potential value is itself a prime number, end the search.
-            if (primeFound(primeCeiling)) {
-                break;
-            }
-        }
-        // Else, decrease the highest potential value by one and continue the search.
-        primeCeiling--;
-    } while (primeCeiling >= 2);
+    std::uint64_t largestPrime = largestPrimeFactor(findPrimeOf);
 
-    // Output the largest prime factor of FIND_PRIME.
-    std::cout << "The largest prime factor of " << FIND_PRIME << " is: " << primeCeiling << '\n';
 
+    // End the execution timer.
+    auto endPoint = std::chrono::high_resolution_clock::now();
+    auto executionTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endPoint - startPoint);
+
+
+    // Output the largest prime factor and the time it took to find it.
+    std::cout << "The largest prime factor of " << findPrimeOf << " is: " << largestPrime << '\n'
+        << "Found in: " << executionTime.count() << " nanoseconds." << '\n';
     return 0;
 }
---
+++ C++/Problem_3/largestPrimeFactor.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+// Function to calculate the largest prime factor of an input integral.
+std::uint64_t largestPrimeFactor (std::uint64_t number);
+
+
+
+// Function to determine if an input integral is prime.
+bool isPrime (std::uint64_t potentialPrime);
